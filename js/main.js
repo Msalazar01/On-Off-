f241a5a66bdc61c753e8f9ed4647a0904ef8997b
Graphic <graphic@SOD-SJH-516-23M.local>
2015-10-02 14:54:19 -0400

    In Class activity

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+var light_on = true;
+
+$('.switch').on('click', function(e) {
 
 
+	$('body').toggleClass('light');
+	$('body').toggleClass('dark');
+
+	$('button').toggleClass('on');
+	$('button').toggleClass('off');
+
+	if ( '.status' != light_on ) {
+		$('.status').text("Hey, who turn off the lights?");
+	}
+
+});
